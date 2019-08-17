abf2d5338bc160e1b74e493455f83d27226e7fa3
jiacheng <jiacheng@Jiacheng-MBP.lan>
2019-08-17 15:54:49 +1000

    Third
    Third commit

--- Slotify 5/includes/handlers/register-handler.php
+++ Slotify 5/includes/handlers/register-handler.php
@@ -28,11 +28,14 @@ if(isset($_POST['registerButton'])) {
 	$email2 = sanitizeFormString($_POST['email2']);
 	$password = sanitizeFormPassword($_POST['password']);
 	$password2 = sanitizeFormPassword($_POST['password2']);
-    // $account=new Account();
-    $wassuccedd=$account->register($username,$firstName,$lastName,$email,$email2,$password,$password2);
-    if($wassuccedd==true){
-    	header("Location:index.php");
-    }
+
+	$wasSuccessful = $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+
+	if($wasSuccessful == true) {
+		$_SESSION['userLoggedIn']=$username;
+		header("Location: index.php");
+	}
+
 }
 
 
